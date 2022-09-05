fddce70034634f5d05144b30341e794802328368
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2022-09-05 12:10:58 -0600

    multiple AR scenes?

--- ar-app/next.config.js
+++ ar-app/next.config.js
@@ -6,7 +6,13 @@ const nextConfig = {
 
 rewrites: async () => [
   {
+    source: "/public/medicine_bow.html",
+    destination: "/pages/api/ar_test.js",
+
     source: "/public/ar_test.html",
+    destination: "/pages/api/ar_test.js",
+
+    source: "/public/yale.html",
     destination: "/pages/api/ar_test.js",
   },
 ],
