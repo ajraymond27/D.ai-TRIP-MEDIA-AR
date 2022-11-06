2553e52e5785146e1764e88886f2da23ce88032a
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2022-11-06 15:08:22 -0700

    idk test

--- ar-app/next.config.js
+++ ar-app/next.config.js
@@ -4,17 +4,17 @@ const nextConfig = {
   swcMinify: true,
 }
 
-rewrites: async () => [
-  {
-    source: "/public/medicine_bow.html",
-    destination: "/pages/api/ar_test.js",
+// rewrites: async () => [
+//   {
+//     source: "/public/medicine_bow.html",
+//     destination: "/pages/api/ar_test.js",
 
-    source: "/public/ar_test.html",
-    destination: "/pages/api/ar_test.js",
+//     source: "/public/ar_test.html",
+//     destination: "/pages/api/ar_test.js",
 
-    source: "/public/yale.html",
-    destination: "/pages/api/ar_test.js",
-  },
-],
+//     source: "/public/yale.html",
+//     destination: "/pages/api/ar_test.js",
+//   },
+// ],
 
 module.exports = nextConfig
